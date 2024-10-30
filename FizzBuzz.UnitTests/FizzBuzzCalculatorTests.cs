0a80bf00626edf90e1ccf7684080c2bae8d2e54a
JotaCe14 <jose.bustamante@dbtarget.pe>
2024-10-30 11:03:21 -0500

    Cycle 1 - Red

---
+++ FizzBuzz.UnitTests/FizzBuzzCalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿namespace FizzBuzz.UnitTests;
+public class FizzBuzzCalculatorTests
+{
+    [Fact]
+    public void Calculate_Should_Return_StringRepresentationOf1_When_Number_Is_1()
+    {
+        //Arrange
+        var sut = new FizzBuzzCalculator();
+
+        //Act
+        string otuput = sut.Calculate(1);
+
+        //Assert
+        Assert.Equal("1", otuput);
+    }
+}
