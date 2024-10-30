a52661b516f7126b24a38a17d091642f649e4a7b
JotaCe14 <jose.bustamante@dbtarget.pe>
2024-10-30 11:10:46 -0500

    Cycle 2 - Red

--- FizzBuzz.UnitTests/FizzBuzzCalculatorTests.cs
+++ FizzBuzz.UnitTests/FizzBuzzCalculatorTests.cs
@@ -13,4 +13,16 @@ public class FizzBuzzCalculatorTests
         //Assert
         Assert.Equal("1", otuput);
     }
+    [Fact]
+    public void Calculate_Should_Return_StringRepresentationOf2_When_Number_Is_2()
+    {
+        //Arrange
+        var sut = new FizzBuzzCalculator();
+
+        //Act
+        string otuput = sut.Calculate(2);
+
+        //Assert
+        Assert.Equal("2", otuput);
+    }
 }
