4515d9ecc55cb2a62709715bfa6cf68a31771c8c
JotaCe14 <jose.bustamante@dbtarget.pe>
2024-10-30 11:17:14 -0500

    Cycle 3 - Refactor Tests

--- FizzBuzz.UnitTests/FizzBuzzCalculatorTests.cs
+++ FizzBuzz.UnitTests/FizzBuzzCalculatorTests.cs
@@ -1,42 +1,19 @@
 ﻿namespace FizzBuzz.UnitTests;
 public class FizzBuzzCalculatorTests
 {
-    [Fact]
-    public void Calculate_Should_Return_StringRepresentationOf1_When_Number_Is_1()
+    [Theory]
+    [InlineData(1, "1")]
+    [InlineData(2, "2")]
+    [InlineData(4, "4")]
+    public void Calculate_Should_Return_StringRepresentation_When_Number_IsNot_MultipleOf3_Or_Is_MultipleOf5(int number, string expectedOuput)
     {
         //Arrange
         var sut = new FizzBuzzCalculator();
 
         //Act
-        string otuput = sut.Calculate(1);
+        string otuput = sut.Calculate(number);
 
         //Assert
-        Assert.Equal("1", otuput);
-    }
-
-    [Fact]
-    public void Calculate_Should_Return_StringRepresentationOf2_When_Number_Is_2()
-    {
-        //Arrange
-        var sut = new FizzBuzzCalculator();
-
-        //Act
-        string otuput = sut.Calculate(2);
-
-        //Assert
-        Assert.Equal("2", otuput);
-    }
-
-    [Fact]
-    public void Calculate_Should_Return_StringRepresentationOf4_When_Number_Is_4()
-    {
-        //Arrange
-        var sut = new FizzBuzzCalculator();
-
-        //Act
-        string otuput = sut.Calculate(4);
-
-        //Assert
-        Assert.Equal("4", otuput);
+        Assert.Equal(expectedOuput, otuput);
     }
 }
